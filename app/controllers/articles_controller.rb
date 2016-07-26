cbd19be7c3571f0486d8f548465b45dacb1e08c3
Shantanu Jain <shantanu14097@iiitd.ac.in>
2016-07-26 18:10:33 +0000

    map

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,11 @@ class ArticlesController < ApplicationController
             config.consumer_key    = "8yEM5tZHe1kgh6pRM8hYMtdsz"
             config.consumer_secret = "zaXl77mTujGJjeNS9lrjjaRcYo2okkMC4lB2F7gF12lQznlEEb"
         end
-        @trends = client.trends(id = 23424848, options = {})
-        
+        #@locid = params[:param1]
+        @latitude = params[:param1]
+        @longitude = params[:param2]
+        @closest = client.trends_closest(options = {:lat => @latitude, :long => @longitude})
+        @locid1 = @closest[0]["woeid"]
+        @trends = client.trends(id = @locid1, opions = {})
     end
 end
